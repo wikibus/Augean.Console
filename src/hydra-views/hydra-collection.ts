8a9a22a0413b089e206fc47375b8308b22e4ea0e
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2016-05-03 19:54:42 +0200

    styling collection element

--- src/hydra-views/hydra-collection.ts
+++ src/hydra-views/hydra-collection.ts
@@ -1,9 +1,5 @@
 import {ObjectGetter} from './hydra-behaviors';
 
-@template(`
-<object-view model="[[getObject(collection, 'http://www.w3.org/ns/hydra/core#member')]]"></object-view>
-<object-view model="[[getObject(collection, 'http://www.w3.org/ns/hydra/core#view')]]"></object-view>
-`)
 @behavior(ObjectGetter)
 @component('hydra-collection')
 class HydraCollection extends polymer.Base {
