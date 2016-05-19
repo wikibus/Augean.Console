716d92167e289a97f1b66ec39e90d62c1a873de3
tpluscode <tpluscode@users.noreply.github.com>
2016-05-19 22:29:47 +0200

    styling op selector

--- src/operation-views/operation-selector.ts
+++ src/operation-views/operation-selector.ts
@@ -5,17 +5,33 @@ import '../../bower_components/paper-tooltip/paper-tooltip.html!';
 
 @component('operation-selector')
 class OperationSelector extends polymer.Base {
-    
+
     @property()
     operations:Array<IOperation>;
+
+    @computed()
+    hasOperations(operations) {
+        return !!operations && operations.length > 0;
+    }
+
+    @observe('hasOperations')
+    updateFabStyle(hasOperations) {
+        this.updateStyles();
+    }
 
     @listen('blur')
     onBlur() {
         this.$.operationToggle.active = false;
     }
 
+    showTooltip() {
+        if (!this.hasOperations) {
+            this.$.noOperationsTooltip.show();
+        }
+    }
+
     getIcon(operation:IOperation) {
-        switch((operation.method || '').toUpperCase()) {
+        switch ((operation.method || '').toUpperCase()) {
             case 'GET':
                 return 'icons:file-download';
             case 'PUT':
