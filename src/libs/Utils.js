2f4a17b741a80987330b45d2d0948acc92117f6b
tpluscode <tpluscode@users.noreply.github.com>
2017-11-03 18:12:24 +0100

    only unique

--- src/libs/Utils.js
+++ src/libs/Utils.js
@@ -1,13 +1,27 @@
 import flatten from 'lodash.flatten';
 
+function uniqBy(arr, predicate) {
+    const cb = typeof predicate === 'function' ? predicate : (o) => o[predicate];
+
+    return [...arr.reduce((map, item) => {
+        const key = cb(item);
+
+        map.has(key) || map.set(key, item);
+
+        return map;
+    }, new Map()).values()];
+}
+
 function getProperties(resource) {
     if(!resource.types || Array.isArray(resource.types) === false){
          return [];
     }
 
-    return resource.types
+    const allProperties = resource.types
                 .map(type => resource.apiDocumentation.getProperties(type))
                 .reduce((acc, val) => [...acc, ...val], []);
+
+    return uniqBy(allProperties, sp => sp.property.id);
 }
 
 export {
